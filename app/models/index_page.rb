de327f0ac4e98dcb706024c8c47c83441847fabe
Michael Kessler <michael.kessler@screenconcept.ch>
2009-07-15 17:20:23 +0200

    Make work with Radiant 0.8.0 and add the possiblity to include a page instead of redirecting

--- app/models/index_page.rb
+++ app/models/index_page.rb
@@ -1,15 +1,38 @@
 class IndexPage < Page 
-  
+
+  #
+  # The Index Page type redirects to the first published child
+  #
   def render
+
     published_children = children.delete_if{|c| c.status_id != 100 }
+
     if !published_children.empty?
-      if defined?(SiteLanguage)  && SiteLanguage.count > 0
-        response.redirect "/#{params[:language]||I18n.code.to_s}#{published_children.first.url}", "302 Found"
+
+      if Radiant::Config["index.page"] && Radiant::Config["index.page"] == 'include'
+
+        # Just render the first published child under the parents url
+        published_children.first.render
+
       else
-        response.redirect published_children.first.url, "302 Found"
+
+        # Prefix url with the site language?
+        if defined?(SiteLanguage)  && SiteLanguage.count > 0
+          redirect_url = (params[:language] || I18n.code.to_s) + published_children.first.url
+        else
+          redirect_url = published_children.first.url
+        end
+
+        # Redirect to a published child page
+        response.headers['Refresh'] = "0; url=#{redirect_url}"
+        ''
       end
+
     else
+
+      # No published children found, so render the page itself
       super
+
     end
   end
   
